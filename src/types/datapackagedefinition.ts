4c3ecab6023eb985d2f18807099aa51555256824
Luca Bassani <baslu93@live.it>
2026-09-14 09:41:45 +0200

    Remove datakit clean command and its helpers

--- src/types/datapackagedefinition.ts
+++ src/types/datapackagedefinition.ts
@@ -1,27 +1,3 @@
-export interface DataPackageDefinitionMetadata {
-  fullName: string;
-  masterLabel: string;
-  dataSpaceDefinitionDevName?: string;
-  deploymentOrder?: string;
-}
-
-export interface DataPackageKitObjectRecord {
-  fullName: string;
-  parentDataPackageKitDefinitionName: string;
-  referenceObjectName: string;
-  referenceObjectType: string;
-}
-
-export interface DataSourceBundleDefinitionMetadata {
-  fullName: string;
-  dataPlatform: string;
-}
-
-export interface DataKitObjectTemplateRecord {
-  fullName: string;
-  entityPayload: Record<string, string>;
-}
-
 export interface DatakitDevopsDeployResponse {
   jobId: string;
 }
